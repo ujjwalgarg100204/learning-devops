4dbc1e16bb87a88458a8d5df50a6b37f05eeb9d4
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2025-01-01 21:31:44 +0530

    initial commit

---
+++ src/main/java/com/ujjwalgarg/learningdevops/LearningDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.ujjwalgarg.learningdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDevopsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearningDevopsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ujjwalgarg/learningdevops/LearningDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujjwalgarg.learningdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDevopsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
